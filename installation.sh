c83bd8b7db6c3034b3805dba6cbddf9428eedfbb
Tristen <TristenS@protonmail.com>
2019-08-03 16:09:23 -0400

    Created installation script. Cleaned up erroneous code in main python file. Created a readme. Created a gitignore.

---
+++ installation.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+mkdir -p ~/bin
+mkdir -p ~/.local/Megadownloader
+cp -f megadownload ~/bin
+cp -f megadownloader.py ~/.local/Megadownloader
+cp -f readme ~/.local/Megadownloader
--- megadownloader.py
+++ megadownloader.py
@@ -1,4 +1,4 @@
-import pyperclip as pyclip
+import pyperclip
 import os
 
 # test_urls = "https://mega.nz/#!7t80kACA!qsmXHvLWE8eq-J5_MSqMKXTced7ur7Djm9CeVmLIdm4 , ''https://mega.nz/#!uxFCWaxR!qfe2sFlvnBxS74NOlmfE7K5SFpfjASL0wn1iaH64YzM"
@@ -6,27 +6,22 @@ import os
 
 
 def html_clipboard_extract():
-    clipboard_contents = pyclip.paste().split()
+    # Scans clipboard for mega.co urls. Did my best to remove any extra non-alphanumeric characters. Could definitely be better in that regard.
+    clipboard_contents = pyperclip.paste().split()
     output_string = ""
     output_urls = []
     start_location = 0
     for string in clipboard_contents:
         if "mega." in string:
             last_dash = string.rfind("-")
-            last_url_char = string.rfind("-")
             for char in string:
                 if char == "H":
                     start_location = string.index("H")
                 elif char == "h":
                     start_location = string.index("h")
+
             output_string = string[start_location:]
-# save the location of the last occurance of / then
-# scan the rest for numbers and letters then stop at the first non
-# letter or int
-            for char in string[last_dash:]:
-                if char.isalpha() == False and char.isdigit() == False:
-                    last_url_char = string.index(char)
-                    break
+
             output_string1 = string.replace('"', "")
             output_string2 = output_string1.replace("'", "")
             output_string3 = output_string2.replace("\"", "")
@@ -36,6 +31,7 @@ def html_clipboard_extract():
 
 
 def downloader(url_list):
+    # Takes a list of urls, uses them as argumets for Megatools' megadl command which will download them one by one.
     for url in url_list:
         os.system(f"megadl '{url}'")
 
