1a900ed22e5ce2a9f1bf2195be09d8a6c52393a0
Tristen <TristenS@protonmail.com>
2019-08-27 15:51:57 -0400

    Add support for an argument specifying the download directory. Change main script to the python script itself as opposed to a bash script that executes the python code. Update installation script and readme as such.

--- megadownloader.py
+++ megadownloader.py
@@ -1,12 +1,10 @@
+#!/usr/bin/env python3
 import pyperclip
 import os
-
-# test_urls = "https://mega.nz/#!7t80kACA!qsmXHvLWE8eq-J5_MSqMKXTced7ur7Djm9CeVmLIdm4 , ''https://mega.nz/#!uxFCWaxR!qfe2sFlvnBxS74NOlmfE7K5SFpfjASL0wn1iaH64YzM"
-# test_urls = " +++= 'https://mega.nz/#!7t80kACA!qsmXHvLWE8eq-J5_MSqMKXTced7ur7Djm9CeVmLIdm4 'https://mega.nz/#!uxFCWaxR!qfe2sFlvnBxS74NOlmfE7K5SFpfjASL0wn1iaH64YzM"
+from sys import argv
 
 
 def html_clipboard_extract():
-    # Scans clipboard for mega.co urls. Did my best to remove any extra non-alphanumeric characters. Could definitely be better in that regard.
     clipboard_contents = pyperclip.paste().split()
     output_string = ""
     output_urls = []
@@ -14,14 +12,20 @@ def html_clipboard_extract():
     for string in clipboard_contents:
         if "mega." in string:
             last_dash = string.rfind("-")
+            last_url_char = string.rfind("-")
             for char in string:
                 if char == "H":
                     start_location = string.index("H")
                 elif char == "h":
                     start_location = string.index("h")
-
             output_string = string[start_location:]
-
+# save the location of the last occurance of / then
+# scan the rest for numbers and letters then stop at the first non
+# letter or int
+            for char in string[last_dash:]:
+                if char.isalpha() == False and char.isdigit() == False:
+                    last_url_char = string.index(char)
+                    break
             output_string1 = string.replace('"', "")
             output_string2 = output_string1.replace("'", "")
             output_string3 = output_string2.replace("\"", "")
@@ -30,14 +34,20 @@ def html_clipboard_extract():
     return output_urls
 
 
-def downloader(url_list):
-    # Takes a list of urls, uses them as argumets for Megatools' megadl command which will download them one by one.
-    for url in url_list:
-        os.system(f"megadl '{url}'")
+def downloader(url_list, location):
+    if location == "":
+        for url in url_list:
+            os.system(f"megadl '{url}'")
+    else:
+        for url in url_list:
+            os.system(f"megadl --path {location} '{url}'")
 
 
 def main():
-    downloader(html_clipboard_extract())
+    if len(argv) < 2:
+        downloader(html_clipboard_extract(), "")
+    else:
+        downloader(html_clipboard_extract(), argv[1])
 
 
 if __name__ == "__main__":
